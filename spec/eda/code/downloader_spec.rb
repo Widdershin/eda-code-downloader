0c6734d3f7b96cad5c98554dc56de3fbf25e6394
Nick Johnstone <ncwjohnstone@gmail.com>
2015-07-29 22:08:28 +1200

    Start setting up Gem

---
+++ spec/eda/code/downloader_spec.rb
@@ -0,0 +1,11 @@
+require 'spec_helper'
+
+describe Eda::Code::Downloader do
+  it 'has a version number' do
+    expect(Eda::Code::Downloader::VERSION).not_to be nil
+  end
+
+  it 'does something useful' do
+    expect(false).to eq(true)
+  end
+end
